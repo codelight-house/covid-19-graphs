3a4ccad2a3aa1fe4e39732e41b09616fbe7ba86b
Grzegorz Marchwinski <yankeepl@gmail.com>
2020-03-01 23:06:12 +0100

    feat(frontend): add map animation

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,6 +1,6 @@
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
-import React from "react";
+import React, {useState} from "react";
 import { render } from "react-dom";
 import {
   useQuery,
@@ -27,10 +27,27 @@ const client = new ApolloClient({
   cache,
 });
 
-const COVID_DATA_ROWS = gql`
+// const COVID_DATA_ROWS = gql`
+//   query timeSeriesByDate($date: String!) {
+//     dataRows(limit: 999, filter: {
+//       date: $date
+//     }) {
+//       id
+//       date
+//       region
+//       subRegion
+//       confirmed
+//       deaths
+//       recovered
+//     }
+//   }
+// `;
+
+const COVID_ALL_DATA_ROWS = gql`
   query {
-    dataRows(limit: 400) {
+    dataRows(limit: 10000) {
       id
+      date
       region
       subRegion
       confirmed
@@ -40,102 +57,140 @@ const COVID_DATA_ROWS = gql`
   }
 `;
 
-function CowidRow(props) {
+const COVID_AVAILABLE_DATES = gql`
+  query {
+    availableDates
+  }
+`;
+
+const CovidAnimationController = () => {
+  const { loading, error, data } = useQuery(COVID_AVAILABLE_DATES);
+  const [ currentDate, setCurrentDate ] = useState(null);
+  const [ isAnimationActive, setAnimationActive ] = useState(true);
+
+  if (loading || !data) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error :(</p>;
+  }
+
+  if (!currentDate) {
+    setCurrentDate(data.availableDates[0]);
+  }
+
+  const selectNextDate = () => {
+    const currentIndex = data.availableDates.indexOf(currentDate);
+    const nextIndex = (currentIndex + 1) % data.availableDates.length;
+    setCurrentDate(data.availableDates[nextIndex]);
+  };
+  let animationTimeout = null;
+  if (isAnimationActive) {
+    animationTimeout = setTimeout(selectNextDate, 200);
+  }
+
+  const dates = data.availableDates.map(date => {
+    return <button key={date} onClick={()=>{
+      setCurrentDate(date);
+    }} style={{fontWeight: (date === currentDate) ? "bold" : "normal"}}> {date} </button>
+  })
   return (
-    <div>
-      <div>region: <strong>{props.dataRow.region}{props.dataRow.subRegion ? ` / ${props.dataRow.subRegion}` : null}</strong></div>
-    </div>
-  )
+    <>
+      <button onClick={() => {
+        setAnimationActive(!isAnimationActive);
+        if (animationTimeout) {
+          clearTimeout(animationTimeout);
+        }
+      }}>{isAnimationActive ? "pause" : "start"}</button>
+      {dates}
+      <div style={{ width: "750px", height: "500px" }}>
+        <CovidDataLoader date={currentDate}/>
+      </div>
+    </>
+  );
 }
 
-function CovidGraph() {
-  const { loading, error, data } = useQuery(COVID_DATA_ROWS);
+function CovidDataLoader(props) {
+  const { loading, error, data } = useQuery(COVID_ALL_DATA_ROWS);
 
   if (loading || !data) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  // return data.dataRows.map((dataRow) => (<CowidRow key={dataRow.id} dataRow={dataRow} />));
-  // data.dataRows.forEach((dataRow) => {
-  //   if (!countriesMap[dataRow.region]) {
-  //     console.log(`region not found in countriesMap: ${dataRow.region}`);
-  //   }
-  // });
+  const filteredData = data.dataRows.filter((row) => {
+    return row.date === props.date;
+  });
+  // console.log(data.dataRows.length, filteredData.length);
   return (
-    <div style={{ width: "500px", height: "500px" }}>
-
-      <ResponsiveChoropleth
-        match={(feature, dataItem) => {
-          return (feature.properties.name === dataItem.region);
-          //  {
-          //   return true;
-          // } else {
-          //   console.log(`Not metched: ${feature.properties.name} !== ${dataItem.region}`);
-          //   return false;
-          // }
-        }}
-        data={data.dataRows}
-        features={worldCountries.features}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        colors="reds"
-        // colors={(value,foo,bar)=>{
-        //   console.log(value,foo,bar)
-        //   return 100
-        // }}
-        domain={[1,500]}
-        unknownColor="#EFE"
-        label="properties.name"
-        value="confirmed"
-        valueFormat="d"
-
-        projectionTranslation={[0.5, 0.5]}
-        projectionRotation={[0, 0, 0]}
-        enableGraticule={true}
-        graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        legends={[
-          // {
-          //   anchor: 'bottom-left',
-          //   direction: 'column',
-          //   justify: true,
-          //   translateX: 20,
-          //   translateY: -100,
-          //   itemsSpacing: 0,
-          //   itemWidth: 94,
-          //   itemHeight: 18,
-          //   itemDirection: 'left-to-right',
-          //   itemTextColor: '#444444',
-          //   itemOpacity: 0.85,
-          //   symbolSize: 18,
-          //   effects: [
-          //     {
-          //       on: 'hover',
-          //       style: {
-          //         itemTextColor: '#000000',
-          //         itemOpacity: 1
-          //       }
-          //     }
-          //   ]
-          // }
-        ]}
-      />
-    </div>)
+    <div style={{ width: "750px", height: "500px" }}>
+      <CovidGraph data={filteredData}/>
+    </div>
+  )
 }
 
-// const data = [
-//   {
-//     "id": "KOR",
-//     "foo": 50000
-//   },
-//   {
-//     "id": "POL",
-//     "foo": 10000,
-//   },
-// ];
+function CovidGraph(props) {
+  return (
+    <ResponsiveChoropleth
+      match={(feature, dataItem) => {
+        return (feature.properties.name === dataItem.region);
+        //  {
+        //   return true;
+        // } else {
+        //   console.log(`Not metched: ${feature.properties.name} !== ${dataItem.region}`);
+        //   return false;
+        // }
+      }}
+      data={props.data}
+      features={worldCountries.features}
+      margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      colors="reds"
+      // colors={(value,foo,bar)=>{
+      //   console.log(value,foo,bar)
+      //   return 100
+      // }}
+      domain={[1, 500]}
+      unknownColor="#FFF"
+      label="properties.name"
+      value="confirmed"
+      valueFormat="d"
+
+      projectionTranslation={[0.5, 0.5]}
+      projectionRotation={[0, 0, 0]}
+      enableGraticule={true}
+      graticuleLineColor="#dddddd"
+      borderWidth={0.5}
+      borderColor="#152538"
+      legends={[
+        // {
+        //   anchor: 'bottom-left',
+        //   direction: 'column',
+        //   justify: true,
+        //   translateX: 20,
+        //   translateY: -100,
+        //   itemsSpacing: 0,
+        //   itemWidth: 94,
+        //   itemHeight: 18,
+        //   itemDirection: 'left-to-right',
+        //   itemTextColor: '#444444',
+        //   itemOpacity: 0.85,
+        //   symbolSize: 18,
+        //   effects: [
+        //     {
+        //       on: 'hover',
+        //       style: {
+        //         itemTextColor: '#000000',
+        //         itemOpacity: 1
+        //       }
+        //     }
+        //   ]
+        // }
+      ]}
+    />
+  )
+}
 
 const App = () => (
   <ApolloProvider client={client}>
-    <CovidGraph />
+    <CovidAnimationController />
   </ApolloProvider>
 );
 
